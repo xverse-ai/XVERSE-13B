d5d7f8e65df3c5427d40704f6ca37c74bfac3f80
pom <502219331@qq.com>
2023-11-06 01:35:38 +0800

    Update demo code.

--- chat_demo.py
+++ chat_demo.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from transformers import AutoModelForCausalLM, AutoTokenizer,GenerationConfig
 
 tokenizer, model = None, None
+system_tpl = None
+exec_tpl = None
 css = """
 .message.user{
 border-color: #BFB0FA !important;
@@ -25,15 +27,21 @@ def init_model(args):
     model.generation_config = GenerationConfig.from_pretrained(args.model_path)
     model = model.eval()
 
-def chat(message, history, request: gr.Request):
+def chat(message, history, system_text, exec_text, request: gr.Request):
     global tokenizer, model
     history = history or []
-    history.append({"role": "user", "content": message})
+    if system_text is not None and system_text != "" and len(history) == 0:
+        history.append({"role": "system", "content": system_text})
+    if exec_text is not None and exec_text != "":
+        history.append({"role": "exec", "content": exec_text})
+    else:
+        history.append({"role": "user", "content": message})
 
     # init
     history.append({"role": "assistant", "content": ""})
     utter_history = []
-    for i in range(0, len(history), 2):
+    start_index = 1 if len(history) % 2 == 1 else 0
+    for i in range(start_index, len(history), 2):
         utter_history.append([history[i]["content"], history[i+1]["content"]])
 
     # chat with stream
@@ -54,7 +62,7 @@ def get_args():
                        help="server port")
     parser.add_argument("--title", type=str, default="XVERSE-13B-Chat",
                        help="server title")
-    parser.add_argument("--model_path", type=str, default="./VERSE-13B-Chat",
+    parser.add_argument("--model_path", type=str, default="./XVERSE-13B-Chat",
                         help="model path")
     parser.add_argument("--tokenizer_path", type=str, default="./XVERSE-13B-Chat",
                         help="Path to the tokenizer.")
@@ -71,6 +79,13 @@ if __name__ == "__main__":
         gr.Markdown("# <center>{}</center>".format(args.title))
         chatbot = gr.Chatbot(label="Chat history", height=650)
         state = gr.State([])
+        advanced_desc = f"<b>system</b><br>输入对话背景知识。<br><b>exec</b><br>agent调用时，输入api执行后的结果。<br>"
+        with gr.Accordion("Advanced settings", open=False):
+            gr.HTML(advanced_desc)
+            with gr.Row():
+                system_text = gr.Textbox(label="system", show_label=True, value=system_tpl)
+            with gr.Row():
+                exec_text = gr.Textbox(label="exec", show_label=True, value=exec_tpl)
 
         with gr.Row():
             text_box = gr.Textbox(label="Message", show_label=False, placeholder="Enter message and press enter")
@@ -80,18 +95,18 @@ if __name__ == "__main__":
             reset_btn = gr.Button(value="Reset")
 
         text_box.submit(fn=chat,
-                        inputs=[text_box, state],
+                        inputs=[text_box, state, system_text, exec_text],
                         outputs=[chatbot, state],
                         api_name="chat")
         submit_btn.click(fn=chat,
-                         inputs=[text_box, state],
+                         inputs=[text_box, state, system_text, exec_text],
                          outputs=[chatbot, state])
 
         # 用于清空text_box
         def clear_textbox():
-            return gr.update(value="")
-        text_box.submit(fn=clear_textbox, inputs=None, outputs=[text_box])
-        submit_btn.click(fn=clear_textbox, inputs=None, outputs=[text_box])
+            return gr.update(value=""), gr.update(value="")
+        text_box.submit(fn=clear_textbox, inputs=None, outputs=[text_box, exec_text])
+        submit_btn.click(fn=clear_textbox, inputs=None, outputs=[text_box, exec_text])
 
         # 用于清空页面和重置state
         def reset():
